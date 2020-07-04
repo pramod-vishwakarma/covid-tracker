cd3b26e13bfe28f93528898dd11a5510423bdc62
Pramod Vishwakarma <pramodvishwakarma1997@gmail.com>
2020-07-03 21:25:51 -0700

    Updates

---
+++ precache-manifest.8ad918b307b59edffbe2c188ac786fd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1dfc18929e32007cc48cba8453fa48",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "7e25f4e86c1745c67ea3",
+    "url": "/covid-tracker/static/css/main.c991d12b.chunk.css"
+  },
+  {
+    "revision": "713697f12cef0314f195",
+    "url": "/covid-tracker/static/js/2.2fdc4ed8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.2fdc4ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e25f4e86c1745c67ea3",
+    "url": "/covid-tracker/static/js/main.7f1868dc.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/corona.d7265326.png"
+  }
+]);
